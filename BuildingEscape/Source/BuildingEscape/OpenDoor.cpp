637e0fd43a151e186eb4aee07632685ba9e83e3f
Cale Curry <c.curry2210@gmail.com>
2018-07-23 16:47:32 -0500

    Created a way to find the playerr pawn without ejecting in Play Mode.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -2,6 +2,7 @@
 
 #include "OpenDoor.h"
 #include "GameFramework/Actor.h"
+#include "Engine/World.h"
 
 
 // Sets default values for this component's properties
@@ -32,6 +33,7 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
+	ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
 	
 	
 	
